28644eb4c83cb139ad296a2779d95b2b3127319b
Wing leung CHOI <wingleungchoi@gmail.com>
2018-10-17 23:56:26 +0800

    mockup basic done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Dropdown from './components/basic/Dropdown';
+import Result from './components/demographic/Result';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Variables
+      <div className="App">
+        <h5>Variables</h5>
         <Dropdown />
+        <Result />
       </div>
     );
   }
