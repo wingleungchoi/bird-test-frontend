cd17edc43068f922eb2b07b7bac9cbbdaaf16a19
Wing leung CHOI <wingleungchoi@gmail.com>
2018-10-17 21:33:34 +0800

    mockup install eslint and bootstrap

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb",
+    "env": {
+      "browser": true,
+      "commonjs": true,
+      "es6": true,
+    },
+    "rules": {
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "no-underscore-dangle": [2, { "allow": ["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] }],
+      "react/prefer-stateless-function": [0],
+    }
+};
