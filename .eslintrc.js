1826e2a265e2a360e5912c90a94690927fc78710
Wing leung CHOI <wingleungchoi@gmail.com>
2018-10-17 22:09:33 +0800

    mockup add ramda and helpr functions

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,23 @@ module.exports = {
       "commonjs": true,
       "es6": true,
     },
+    "settings": {
+      "import/resolver": {
+        "node": {
+          "paths": ["src"]
+        }
+      }
+    },
     "rules": {
       "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
       "no-underscore-dangle": [2, { "allow": ["__REDUX_DEVTOOLS_EXTENSION_COMPOSE__"] }],
       "react/prefer-stateless-function": [0],
+      "comma-dangle": ["error", {
+        "arrays": "never",
+        "objects": "always",
+        "imports": "never",
+        "exports": "never",
+        "functions": "ignore"
+      }],
     }
 };
--- src/components/basic/Dropdown.js
+++ src/components/basic/Dropdown.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { DropdownButton, MenuItem, ButtonToolbar } from 'react-bootstrap';
 
+import { capitalize } from 'helpers/stringHelper';
+
 // TODO proived by backend
 const DEMONGRAPHIC_COLUMNS = ['age', 'class of worker', 'industry code', 'occupation code', 'education', 'wage per hour', 'last education', 'marital status', 'major industry code', 'major occupation code', 'mace', 'hispanice', 'sex', 'member of labor', 'reason for unemployment', 'fulltime', 'capital gain', 'capital loss', 'dividends', 'income tax liability', 'previous residence region', 'previous residence state', 'household-with-family', 'household-simple', 'weight', 'msa-change', 'reg-change', 'within-reg-change', 'lived-here', 'migration prev res in sunbelt', 'num persons worked for employer', 'family members under 118', 'father birth country', 'mother birth country', 'birth country', 'citizenship', 'own business or self employed', 'fill questionnaire for veteran\'s admin', 'veterans benefits', 'weeks worked in year', 'year', 'salary range'];
 
@@ -14,11 +16,10 @@ const Dropdown = () => (
     >
       {DEMONGRAPHIC_COLUMNS.map((column, index) => {
         const eventKey = `column-${index}`;
-        return <MenuItem key={eventKey} eventKey={eventKey}>{column}</MenuItem>
+        return <MenuItem key={eventKey} eventKey={eventKey}>{capitalize(column)}</MenuItem>
       })}
     </DropdownButton>
   </ButtonToolbar>
 );
 
 export default Dropdown;
-
